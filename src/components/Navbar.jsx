b3df7d49fd9e90b5a673430c96e1e3f3925d57c2
muhammad230 <ali.khansmr3@gmail.com>
2026-02-20 10:52:31 +0500

    add some css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-5 flex justify-between items-center">
-        <div className="text-white font-bold">Sneakers</div>
-        <ul className="flex space-x-4 hover:space-x-6 transition-all duration-300">
-          <li><a href="#" className="text-white hover:text-gray-300 hover:border-b-2 border-orange-500">Collection</a></li>
-          <li><a href="#" className="text-white hover:text-gray-300 hover:border-b-2 border-orange-500">Men</a></li>
-          <li><a href="#" className="text-white hover:text-gray-300 hover:border-b-2 border-orange-500">Women</a></li>
-          <li><a href="#" className="text-white hover:text-gray-300 hover:border-b-2 border-orange-500">About</a></li>
-          <li><a href="#" className="text-white hover:text-gray-300 hover:border-b-2 border-orange-500">Contact</a></li>
+    <nav className="bg-white p-5 flex justify-between items-center border-b border-[#ddd]">
+        <div className="text-black font-bold text-3xl">Sneakers</div>
+        <ul className="flex space-x-4 hover:space-x-6 transition-all duration-300 text-xl gap-4">
+          <li><a href="#" className="text-gray-800 hover:text-black hover:border-b-2 border-orange-500">Collection</a></li>
+          <li><a href="#" className="text-gray-800 hover:text-black hover:border-b-2 border-orange-500">Men</a></li>
+          <li><a href="#" className="text-gray-800 hover:text-black hover:border-b-2 border-orange-500">Women</a></li>
+          <li><a href="#" className="text-gray-800 hover:text-black hover:border-b-2 border-orange-500">About</a></li>
+          <li><a href="#" className="text-gray-800 hover:text-black hover:border-b-2 border-orange-500">Contact</a></li>
         </ul>
+        <div className="h-8 w-8 cursor-pointer">
+          <img src="/images/shopping-cart-icon-free-vector.jpg" alt="" />
+        </div>
       
     </nav>
   );
