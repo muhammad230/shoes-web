263d5e24f8b097552f9cf998c4a313a52cc9641c
muhammad230 <ali.khansmr3@gmail.com>
2026-02-21 13:25:09 +0500

    add button and use usestate

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Main = () => {
+
+    const [count, setcount] = useState(1);
   return (
     <div className="max-w-6xl mx-auto p-10 grid md:grid-cols-2 gap-12 items-center">
 
@@ -64,6 +66,12 @@ const Main = () => {
           </div>
           <p className="text-gray-400 line-through">$250.00</p>
         </div>
+        {/* Button */}
+            <div className="flex items-center gap-4">
+              <button className="bg-gray-200 hover:bg-gray-300 w-8 h-8 rounded-full flex items-center justify-center" onClick={() => setcount(count > 1 ? count - 1 : 1)}>-</button>
+              <span className="font-bold">{count}</span>
+              <button className="bg-gray-200 hover:bg-gray-300 w-8 h-8 rounded-full flex items-center justify-center" onClick={() => setcount(count + 1)}>+</button>
+            </div>
 
         {/* Add to Cart Button */}
         <button className="bg-orange-500 hover:bg-orange-600 text-white px-10 py-3 rounded-lg font-bold shadow-md">
